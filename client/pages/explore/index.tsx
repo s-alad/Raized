8dab81a5a9a7938c7a2e1f5ae776f1542268e53a
s-alad <n7.saad@gmail.com>
2024-04-28 08:01:51 -0400

    explore

--- client/pages/explore/index.tsx
+++ client/pages/explore/index.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import s from "./explore.module.scss";
 import { useRouter } from "next/router";
 import { CVAR } from "@/utils/constant";
 import { useAuth } from "@/authentication/authcontext";
+import Project from "@/models/project";
+import FullProject from "@/components/project/full/fullproject";
+import Loader from "@/components/loader/loader";
 
 export default function Explore() {
 
@@ -10,8 +13,11 @@ export default function Explore() {
 
     let { search } = router.query;
     const { user, raiser, connect } = useAuth();
+    let [loading, setLoading] = useState(false);
+    let [projects, setProjects] = useState<Project[]>();
 
     async function lookup() {
+        setLoading(true);
         console.log("searching for " + search);
 
         const searchres = await fetch(`${CVAR}/projects/get-projects-by-search?search=${search}`, {
@@ -24,6 +30,8 @@ export default function Explore() {
         });
         const data = await searchres.json();
         console.log(data);
+        setProjects(data.projects as Project[]);
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -31,12 +39,41 @@ export default function Explore() {
         if (search) {
             console.log("searching for " + search);
             lookup()
+        } else {
+            setLoading(true);
+            fetch(`${CVAR}/projects/get-projects`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "publickey": `${raiser?.publickey}`,
+                    "signature": `${raiser?.signature}`,
+                }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    setProjects(data.projects as Project[]);
+                    setLoading(false);
+                })
         }
     }, [search]);
 
+    if (loading) {
+        return <Loader />
+    }
+
     return (
-        <div className={s.explore}>
+        <main className={s.explore}>
             <h1>Explore</h1>
-        </div>
+            <section className={s.projects}>
+                {
+                        projects?.map((project: Project) => {
+                            return (
+                                <div className={s.project}><FullProject key={project.projectuid} project={project} /></div>
+                            )
+                        })
+                }
+            </section>
+        </main>
     )
 }
